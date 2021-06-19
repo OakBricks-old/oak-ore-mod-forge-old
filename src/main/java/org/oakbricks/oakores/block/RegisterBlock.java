54e4c3715ae4f50a5b352357468a5af23f098276
OakBricks <oakbricks@gmail.com>
2021-06-18 22:52:32 -0500

    use an example to see if forge is ignorant or i just suck at coding

--- src/main/java/org/oakbricks/oakores/block/RegisterBlock.java
+++ src/main/java/org/oakbricks/oakores/block/RegisterBlock.java
@@ -16,6 +16,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegisterBlock {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, OakOres.MOD_ID);
 
-    public static final RegistryObject<Block> PURPI_ORE = BLOCKS.register("purpi_ore",
-            () -> new Block(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().harvestLevel(3).strength(13f, 25f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS
+            .register("example_block",
+                    () -> new Block(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_BLUE)
+                            .strength(15f, 30f).harvestTool(ToolType.PICKAXE).harvestLevel(2)
+                            .sound(SoundType.METAL).requiresCorrectToolForDrops()));
+
+
+    //public static final RegistryObject<Block> PURPI_ORE = BLOCKS.register("purpi_ore",
+    //        () -> new Block(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().harvestLevel(3).strength(13f, 25f).sound(SoundType.STONE)));
 }
