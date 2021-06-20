c0b3d0ef4ec1b612caa23775525ec1f7211685b9
OakBricks <oakbricks@gmail.com>
2021-06-20 00:19:09 -0500

    tools

---
+++ src/main/java/org/oakbricks/oakores/tools/purpi/PurpiToolMaterial.java
@@ -0,0 +1,58 @@
+package org.oakbricks.oakores.tools.purpi;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import org.oakbricks.oakores.item.RegisterItem;
+
+import java.util.function.Supplier;
+
+public enum PurpiToolMaterial implements IItemTier {
+
+    PURPI_TOOL(4, 6500, 8, 9, 5, () -> Ingredient.of(RegisterItem.PURPI.get()));
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Ingredient repairMaterial;
+
+    PurpiToolMaterial(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial.get();
+    }
+
+    @Override
+    public int getUses() {
+        return this.maxUses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return this.efficiency;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getLevel() {
+        return this.harvestLevel;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+}
